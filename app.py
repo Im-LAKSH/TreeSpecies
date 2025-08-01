d4a8bcb662c04be3bd3b4d2a4b80568aba9782bf
Im-LAKSH <157018135+Im-LAKSH@users.noreply.github.com>
2025-08-02 00:12:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,19 @@ from PIL import Image
 import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
+import json
+
+with open("class_indices.json", "r") as f:
+    class_indices = json.load(f)
+
+class_names = [None] * len(class_indices)
+for class_name, index in class_indices.items():
+    class_names[index] = class_name
 
 model = tf.keras.models.load_model("best_model.h5")
-class_names = ['nilgiri', 'coconut', 'vad', 'gulmohor', 'sitafal', 'kesudo', 'bili', 'sugarcane', 'gunda', 'saptaparni', 'pilikaren', 'kanchan', 'motichanoti', 'shirish', 'sonmahor', 'babul', 'amla', 'banyan', 'cactus', 'bamboo', 'simlo', 'jamun', 'mango', 'other', 'pipal', 'neem', 'champa', 'khajur', 'garmalo', 'asopalav']
 
 def preprocess_image(image):
+    image = image.convert("RGB")
     image = image.resize((224, 224))
     img_array = np.array(image) / 255.0
     return np.expand_dims(img_array, axis=0)
@@ -17,7 +25,6 @@ def plot_prediction_bar(predictions):
     top_indices = np.argsort(confidences)[::-1][:3]
     top_classes = [class_names[i] for i in top_indices]
     top_scores = confidences[top_indices]
-
     fig, ax = plt.subplots()
     bars = ax.barh(top_classes[::-1], top_scores[::-1], color="lightgreen")
     ax.set_xlim([0, 1])
@@ -35,12 +42,12 @@ uploaded_file = st.file_uploader("Drag & drop or browse an image...", type=["jpg
 
 if uploaded_file:
     image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.image(image, caption="Uploaded Image", use_container_width=True)
     img = preprocess_image(image)
-    prediction = model.predict(img)
+    with st.spinner("Classifying..."):
+        prediction = model.predict(img)
     top_class = np.argmax(prediction)
     confidence = np.max(prediction)
-
     st.success(f"Prediction: {class_names[top_class]} with {confidence*100:.2f}% confidence")
     st.subheader("Top-3 Prediction Confidence")
     plot_prediction_bar(prediction)
